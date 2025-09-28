d7de4b6f6eaae274d1af661db9786445dc9ac038
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-09-27 20:10:23 -0500

    feat: add users.json file and endpoint to fetch users
    Add a new JSON file containing user data and implement a GET endpoint to serve this data. The endpoint reads from the file and returns the parsed JSON content.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const fs = require("fs");
+const path = require("path");
+const usersFilePath = path.join(__dirname, "users.json");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
       <h1>Curso Express.js V3</h1>
       <p>Esto es una aplicación node.js con express.js</p>
@@ -17,14 +21,14 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.get('/users/:id', (req, res) => {
+app.get("/users/:id", (req, res) => {
   const userId = req.params.id;
   res.send(`Mostrar información del usuario con ID: ${userId}`);
 });
 
-app.get('/search', (req, res) => {
-  const terms = req.query.termino || 'No especificado';
-  const category = req.query.categoria || 'Todas';
+app.get("/search", (req, res) => {
+  const terms = req.query.termino || "No especificado";
+  const category = req.query.categoria || "Todas";
 
   res.send(`
       <h2>Resultados de Busqueda:</h2>
@@ -33,28 +37,38 @@ app.get('/search', (req, res) => {
     `);
 });
 
-app.post('/form', (req, res) => {
-  const name = req.body.nombre || 'Anónimo';
-  const email = req.body.email || 'No proporcionado';
+app.post("/form", (req, res) => {
+  const name = req.body.nombre || "Anónimo";
+  const email = req.body.email || "No proporcionado";
   res.json({
-    message: 'Datos recibidos',
+    message: "Datos recibidos",
     data: {
       name,
-      email
-    }
+      email,
+    },
   });
 });
 
-app.post('/api/data', (req, res) => {
+app.post("/api/data", (req, res) => {
   const data = req.body;
 
   if (!data || Object.keys(data).length === 0) {
-    return res.status(400).json({ error: 'No se recibieron datos' });
+    return res.status(400).json({ error: "No se recibieron datos" });
   }
 
   res.status(201).json({
-    message: 'Datos JSON recibidos',
-    data
+    message: "Datos JSON recibidos",
+    data,
+  });
+});
+
+app.get("/users.json", (req, res) => {
+  fs.readFile(usersFilePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al leer el archivo" });
+    }
+    const users = JSON.parse(data);
+    res.json(users);
   });
 });
 
